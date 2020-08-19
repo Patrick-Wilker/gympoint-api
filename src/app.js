68c533cfa323ec8a3e3542e1d5f49a3bc15c2bb1
Patrick Wilker Vieira Mota <patrickvieiramota@gmail.com>
2020-08-19 16:43:35 -0300

    Handling errors with Sentry.io

--- src/app.js
+++ src/app.js
@@ -1,5 +1,12 @@
 import express from 'express'
+
+import 'express-async-errors' // para o sentry conseguir ver os erros que ocorre no async await . Deve ser importado antes das rotas
+
 import routes from './routes'
+
+import * as Sentry from '@sentry/node'
+import sentryConfig from './config/sentry'
+import Youch from 'youch' // deixar mais bonitos os erros para os devs
 
 import './database/index'
 
@@ -7,17 +14,32 @@ class App{
     constructor(){
         this.server = express()
 
+        Sentry.init(sentryConfig)
+
         this.middlewares()
         this.routes()
+        this.exceptionHandler()
     }
 
     middlewares(){
+        this.server.use(Sentry.Handlers.requestHandler());
+
         this.server.use(express.json())
 
     }
 
     routes(){
         this.server.use(routes)
+
+        this.server.use(Sentry.Handlers.errorHandler());
+    }
+
+    exceptionHandler(){
+        this.server.use(async (err, req, res, nex)=>{
+            const errors = await new Youch(err, req).toJSON()
+
+            return res.status(500).json(errors)
+        })
     }
 
 }
